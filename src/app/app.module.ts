2c09ab70c5ae8f36da26d228a02c1b440b2fe0b4
carlosrbta <carlosrbta@gmail.com>
2018-05-17 18:37:25 -0300

    Include pages and routes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { rootRouterConfig } from './app.routes';
 
 import { AppComponent } from './app.component';
+import { AboutComponent } from './about/about.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AboutComponent,
+    HomeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(rootRouterConfig, { useHash: false })
   ],
   providers: [],
   bootstrap: [AppComponent]
